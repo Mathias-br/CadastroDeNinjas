ba85721d089c0ef21c36f4c5baff7f129f203136
Matheus <matheusalexandre1616@gmail.com>
2025-06-11 15:13:20 -0300

    t oCria uma entidade no banco de dados apartir do NinjaModel aula[3]
    src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java:wq

--- src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CadastroDeNinjasApplication {
+public class NinjaController {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CadastroDeNinjasApplication.class, args);
+		SpringApplication.run(NinjaController.class, args);
 	}
 
 
