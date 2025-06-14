f0c5965bc24fde19e49833b87d0a42ce9c0908ab
Matheus <matheusalexandre1616@gmail.com>
2025-06-13 22:30:35 -0300

    feat: Coloca o Lombock na classe para abstração

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjass/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjass/NinjaModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.Ninjass;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 //TODO JPA = Java Persiste API
 
 @Table(name = "tb_cadastro")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 
 public class NinjaModel {
 
@@ -25,36 +31,5 @@ public class NinjaModel {
 
     //private List<MissoesModel> missoes;
 
-    public NinjaModel(){
 
-    }
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
